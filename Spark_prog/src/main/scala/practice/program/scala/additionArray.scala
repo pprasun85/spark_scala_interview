1538140ec061c908be00151aac6a009892845960
PremPrasun <pprasun85@gmail.com>
2023-09-08 17:16:14 +0530

    added 1 file

---
+++ Spark_prog/src/main/scala/practice/program/scala/additionArray.scala
@@ -0,0 +1,19 @@
+package com.interviewques
+
+object additionArray{
+  def main(args: Array[String]): Unit =
+  {
+    val arr = Array(112,34,56,78,55,99)
+    val input = 177
+    findTotal(arr, input)
+    var x = 0
+    var y = 0
+  }
+  def findTotal (arr : Array[Int], input: Int): Unit =
+  {
+
+  }
+  // your code
+  // X + Y = input
+  // what is the value of X and Y from the array ?
+}
